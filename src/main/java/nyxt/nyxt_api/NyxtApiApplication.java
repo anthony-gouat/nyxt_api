268e442ed68e68e5fa6045bb67e0a0bed71e41fc
anthony-gouat <Anthony.gouat@gmail.com>
2022-04-02 11:08:53 +0200

    first commit

---
+++ src/main/java/nyxt/nyxt_api/NyxtApiApplication.java
@@ -0,0 +1,13 @@
+package nyxt.nyxt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyxtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyxtApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nyxt/nyxt_api/NyxtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nyxt.nyxt_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyxtApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
